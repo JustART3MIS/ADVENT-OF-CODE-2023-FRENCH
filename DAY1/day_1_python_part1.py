3d06e82e8c2d58b1f61bd12c9c6f600d07e5e0f5
Art3mis <114515153+JustART3MIS@users.noreply.github.com>
2023-12-06 09:32:44 +0100

    A publié jour_1_python_part1.py

--- DAY1/day_1_python_part1.py
+++ DAY1/day_1_python_part1.py
@@ -1,68 +1,82 @@
-with open("input_day_1.txt", "r") as f:
-    user_input = [line for line in f.readlines()]
+###################################################################################################
+###				ADVENT 			OF			 CODE :			 DAY 1 			PART 1				###
+###################################################################################################
+
+# Objectif du Jour 1  :
+# Vos valeurs données par le site est un fichier texte de 1000 lignes contenant des mots fait de lettres et de chiffes. Votre objectif est de
+# créer une fonction qui sélectionne tous les chiffres du mot, assemble le premier et le dernier de chaque mot pour former un nouveau nombre, puis
+# renvoir la somme de tous les nombres.
+
+# Bonne chance ! 
+
+# Evidemment, si votre objectif est de vous améliorer, pensez a regarder mon programme de manière inspirative, sans le copier. Essayez de le faire à votre manière !
+
+
+with open("input_day_1.txt", "r") as f: # Permet d'importer le fichier .txt contenant votre input du jour
+    user_input = [line for line in f.readlines()] # Contient dans la variable 'user_input' le contenu du fichier entier
 
 
 def trebuchet(words_list):
-    """Puts the first and last number that contains a word to
-    make another number, then sums all the new values.
+    """Assemble le premier et le dernier chiffre d'un mot afin de constituer un nombre, puis fait la somme de tous les nouveaux nombres.
 
-    Args:
-        words_list (str): The input of the day put under a str format
+    Entrées:
+        words_list (str): La liste des mots fournis pas Advent Of Code
     """
 
-  # Creates a list that will be used to store the new values before making the sum of them
+  # Crée un tableau qui servira à stocker les nouveaux nombres pour en faire la somme ensuite
     list_values = []
   
-    for selected_word in words_list: # Selects each word of the input at every iteration
+    for selected_word in words_list: # A chaque passage de boucle, sélectionne un mot du fichier et le stocke dans la variable selected_word
         
-        # print(selected_word) -- If you want to verify that your programs selects correctly the 
-        #                         word
+        # print(selected_word) -- Ce genre de lignes vous permet si vous souhaitez d'observer
+		#							si votre programme fonctionne bien
          
-        # Creates a list to stock the numbers in the word, will be reset at every change of word
+        # Crée un tableau pou rstocker les chiffres du mot dans l'ordre ou ils apparaissent.
+		# Le tableau sera réinitialisé a chaque changement de mot
         list_numbers = []
 
-        for caracter in selected_word: # Selects each caracter of the word
+        for caracter in selected_word: # Selectionne un caractère du mot un par un
           try : 
-            # Turns the caracter into an int
+            # Change le caractère en integer ( etait un chiffre mais en str )
             int(caracter)
-            # Adds the integer to the list 'list_numbers'
+            # Ajoute le caractère au tableau 'list_numbers'
             list_numbers.append(caracter)
 
-          # If a ValueError comes, this means the caracter is a str, so we won't do anything.
+          # Si le programme renvoie une ValueError, c'est que le caractère est une lettre et non un chiffre on le passe donc.
           except ValueError: 
-            pass # Permits the program to skip a line
+            pass # Passe la ligne de code ( remplit le vide d'actions )
             
-        # print(list_numbers) -- If you want to verify the list is well done for your tests
+        # print(list_numbers) 
 
-      # If the word only contains a single number
+      # Si le mot contient seulement un seul nombre
         if len(list_numbers) == 1: 
 
-          # Adds the number two times to make a new number
-          number_1 = str(list_numbers[0]) # Turns the integer to a str to concatenate them
+          # Ajout le nombre deux fois au tableau
+          number_1 = str(list_numbers[0]) # Change le chiffre d'un int à un str pour pouvoir le concaténer avec l'autre
           number_2 = str(list_numbers[0])
           
-          # print("First number = " + number_1) -- If you want to verify you weren't wrong somewhere
-          # print("Last number = " + number_2)  -- If you want to verify you weren't wrong somewhere
+          # print("Premier nombre = " + number_1) 
+          # print("Dernier nombre = " + number_2) 
 
-          value = number_1 + number_2 # Concatenates both values to maje a new value
-          # print("Adding " + value) -- If you want to verify you weren't wrong somewhere
+          value = number_1 + number_2 # Concatène les deux chiffres pour créer le nouveau nombre
+          # print("Ajoute " + value + " au tableau) 
 
 
-          list_values.append(int(value)) # Adds the new number to the list 'list_numbers'
+          list_values.append(int(value)) # Ajoute le nouveau nombre au tableau 'list_numbers'
 
-        elif len(list_numbers) > 1: # If there's more than one number in the word
+        elif len(list_numbers) > 1: # S'il y à plus d'un chiffre dans le mot
           
-          number_1 = str(list_numbers[0]) # Turns the first number of the list into an str
-          number_2 = str(list_numbers[len(list_numbers)-1]) # Turns the last number of the list into an str
-          #print("First number = " + number_1)  -- If you want to verify you weren't wrong somewhere
-          #print("Last number = " + number_2)   
+          number_1 = str(list_numbers[0]) # Change le premier chiffre d'un int à un str pour pouvoir le concaténer avec l'autre
+          number_2 = str(list_numbers[len(list_numbers)-1]) # Change le dernier chiffre d'un int à un str pour pouvoir le concaténer avec l'autre
+          # print("Premier nombre = " + number_1) 
+          # print("Dernier nombre = " + number_2)    
           
-          value = number_1 + number_2 #Concatenates the numbers to form a new one
+          value = number_1 + number_2 # Concatène les deux chiffres pour créer le nouveau nombre
           #print("Adding " + value)
 
-          list_values.append(int(value)) # Adds the new number to the list 'list_numbers'
+          list_values.append(int(value)) # Ajoute le nouveau nombre au tableau 'list_numbers'
     
-    return(sum(list_values))
+    return(sum(list_values)) # Renvoie la somme de toutes les valeurs du tableau
 
 
 
